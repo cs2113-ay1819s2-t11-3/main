28e1e3d068d2776d2981bd159df0364b20fc34ed
yushao2 <p.yushao2@gmail.com>
2019-04-22 14:12:19 +0800

    Removed irrelevant test case

--- src/test/java/seedu/budgeteer/logic/commands/EditCommandTest.java
+++ src/test/java/seedu/budgeteer/logic/commands/EditCommandTest.java
@@ -106,18 +106,6 @@ public class EditCommandTest {
     }
 
     @Test
-    public void execute_duplicateEntryFilteredList_failure() {
-        showEntryAtIndex(model, INDEX_FIRST_ENTRY);
-
-        // edit entry in filtered list into a duplicate in budgeteer book
-        Entry entryInList = model.getAddressBook().getEntryList().get(INDEX_SECOND_ENTRY.getZeroBased());
-        EditCommand editCommand = new EditCommand(INDEX_FIRST_ENTRY,
-                new EditEntryDescriptorBuilder(entryInList).build());
-
-        assertCommandFailure(editCommand, model, commandHistory, EditCommand.MESSAGE_DUPLICATE_ENTRY);
-    }
-
-    @Test
     public void execute_invalidEntryIndexUnfilteredList_failure() {
         Index outOfBoundIndex = Index.fromOneBased(model.getFilteredEntryList().size() + 1);
         EditEntryDescriptor descriptor = new EditEntryDescriptorBuilder().withName(VALID_NAME_BOB).build();
